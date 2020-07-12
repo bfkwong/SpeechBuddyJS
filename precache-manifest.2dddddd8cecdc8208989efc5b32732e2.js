bacbe6cf7a8689756fbd358bf0920047242d15ca
Bryan Kwong <bfkwong@calpoly.edu>
2020-07-11 17:26:12 -0700

    Updates

---
+++ precache-manifest.2dddddd8cecdc8208989efc5b32732e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e52af35b22b17fb248ac4858a6efb4b",
+    "url": "/SpeechBuddyJS/index.html"
+  },
+  {
+    "revision": "692c082a6c6ea2e0590c",
+    "url": "/SpeechBuddyJS/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d4dc03e5c4b8e66c9e1a",
+    "url": "/SpeechBuddyJS/static/css/main.2b18ca31.chunk.css"
+  },
+  {
+    "revision": "692c082a6c6ea2e0590c",
+    "url": "/SpeechBuddyJS/static/js/2.d606822d.chunk.js"
+  },
+  {
+    "revision": "6e368b0f9fe59ec328eb42840ef12968",
+    "url": "/SpeechBuddyJS/static/js/2.d606822d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4dc03e5c4b8e66c9e1a",
+    "url": "/SpeechBuddyJS/static/js/main.041d2a21.chunk.js"
+  },
+  {
+    "revision": "a3ea3735b6c207e295e5",
+    "url": "/SpeechBuddyJS/static/js/runtime-main.dd5a16ed.js"
+  }
+]);
