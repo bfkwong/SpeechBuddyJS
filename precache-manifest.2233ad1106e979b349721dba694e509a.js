7a1cdf44af716de2b7e96a6b206ab460655ac622
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-26 20:43:53 +0000

    Deploy bfkwong/SpeechBuddyJS to github.com/bfkwong/SpeechBuddyJS.git:gh-pages

---
+++ precache-manifest.2233ad1106e979b349721dba694e509a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567673b3e5c5ea4de2d24ab79de3b12e",
+    "url": "/SpeechBuddyJS/index.html"
+  },
+  {
+    "revision": "4019ddba21377cd2170e",
+    "url": "/SpeechBuddyJS/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8530fc3730550f4fb749",
+    "url": "/SpeechBuddyJS/static/css/main.b5479812.chunk.css"
+  },
+  {
+    "revision": "4019ddba21377cd2170e",
+    "url": "/SpeechBuddyJS/static/js/2.7730eb55.chunk.js"
+  },
+  {
+    "revision": "6e368b0f9fe59ec328eb42840ef12968",
+    "url": "/SpeechBuddyJS/static/js/2.7730eb55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8530fc3730550f4fb749",
+    "url": "/SpeechBuddyJS/static/js/main.27051fbe.chunk.js"
+  },
+  {
+    "revision": "a3ea3735b6c207e295e5",
+    "url": "/SpeechBuddyJS/static/js/runtime-main.dd5a16ed.js"
+  }
+]);
--- src/redux/reducer/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-
-import textStoreReducer from "./textStoreReducer";
-
-export default combineReducers({ textStore: textStoreReducer });
