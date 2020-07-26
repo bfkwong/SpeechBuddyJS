6e204a4c7c71f03c855113e163b53a7f7c57e51f
Bryan Kwong <bfkwong@calpoly.edu>
2020-07-26 12:25:20 -0700

    Updates

---
+++ precache-manifest.24bccb674979688ef2e4e0fa3c469e9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ba7b6bd4034418c9fad39ebab0c933",
+    "url": "/SpeechBuddyJS/index.html"
+  },
+  {
+    "revision": "03e25e94ec37f159e056",
+    "url": "/SpeechBuddyJS/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ee0e28841b2f5df884de",
+    "url": "/SpeechBuddyJS/static/css/main.96a688da.chunk.css"
+  },
+  {
+    "revision": "03e25e94ec37f159e056",
+    "url": "/SpeechBuddyJS/static/js/2.a8908770.chunk.js"
+  },
+  {
+    "revision": "6e368b0f9fe59ec328eb42840ef12968",
+    "url": "/SpeechBuddyJS/static/js/2.a8908770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0e28841b2f5df884de",
+    "url": "/SpeechBuddyJS/static/js/main.d38dd3a7.chunk.js"
+  },
+  {
+    "revision": "a3ea3735b6c207e295e5",
+    "url": "/SpeechBuddyJS/static/js/runtime-main.dd5a16ed.js"
+  }
+]);
--- src/redux/action/textStore.js
+++
@@ -1,8 +0,0 @@
-export const WRITE_TEXT = "WRITE_TEXT";
-export const writeText = (textArray) => {
-  return {
-    type: WRITE_TEXT,
-    meta: null,
-    payload: textArray
-  };
-};
