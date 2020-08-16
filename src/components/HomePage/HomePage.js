c7d267adeae6472a3e5b33dff2d169efd4b3a061
Bryan Kwong <bfkwong@calpoly.edu>
2020-08-15 21:08:06 -0700

    fix: dom errors in homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,29 +4,29 @@ import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
     <div>
-      <header class="header">
+      <header>
         <h1>Welcome to SpeechBuddy</h1>
         <h6>The Best Text Analyzer for Students and Teachers Alike</h6>
       </header>
-      <header class="overview">
+      <header>
         <h2>Speech Buddy Overview</h2>
         <p>
           Speech Buddy is a text and speech analysis toolkit for quick
           statistics and visualizations on any given text. SpeechBuddy is
           comprised of three major text analysis tools:
-          <ul>
-            <li>
-              <Link to="/SpeechBuddyJS/single_text">Single Text Analysis</Link>
-            </li>
-            <li>
-              <Link to="/SpeechBuddyJS/two_txt_plag">Two Text Analysis</Link>
-            </li>
-            <li>
-              <Link to="/SpeechBuddyJS/multi_text">Multi Text Analysis</Link>
-            </li>
-          </ul>
         </p>
-        <h2 class="reviews">Satisfied User Reviews</h2>
+        <ul>
+          <li>
+            <Link to="/SpeechBuddyJS/single_text">Single Text Analysis</Link>
+          </li>
+          <li>
+            <Link to="/SpeechBuddyJS/two_txt_plag">Two Text Analysis</Link>
+          </li>
+          <li>
+            <Link to="/SpeechBuddyJS/multi_text">Multi Text Analysis</Link>
+          </li>
+        </ul>
+        <h2>Satisfied User Reviews</h2>
       </header>
     </div>
   );
