b386c50ff215f628266508965abad529ac1aff06
stevengandham <stevengandham@gmail.com>
2020-08-19 12:47:27 -0700

    Fixed Links

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-
     <div>
       <header class = "header">
         <h1>Welcome to SpeechBuddy</h1>
@@ -16,28 +15,30 @@ export default function HomePage() {
             Speech Buddy is a text and speech analysis toolkit for quick statistics and visualizations on any given text.
             SpeechBuddy is comprised of three major text analysis tools:
             <ul>
-              <li><Link to="/single_text">Single Text Analysis</Link></li>
-              <li><Link to="/two_txt_plag">Two Text Analysis</Link></li>
-              <li><Link to="/multi_text">Multi Text Analysis</Link></li>
+              <li><Link to="/SpeechBuddyJS/single_text">Single Text Analysis</Link></li>
+              <li><Link to="/SpeechBuddyJS/two_txt_plag">Two Text Analysis</Link></li>
+              <li><Link to="/SpeechBuddyJS/multi_text">Multi Text Analysis</Link></li>
             </ul>
-            as well as a Speech-To-Text Analysis feature:
+            as well as a Speech-To-Text Analysis feature in Single Text Analysis:
             <ul>
-              <li>Speech Analysis</li>
+              <li><Link to="/SpeechBuddyJS/single_text">Single Text Analysis</Link></li>
             </ul>
           </p>
         <h2 class="reviews">Satisfied User Reviews</h2>
-        <p>
-          SpeechBuddy is wonderful! I used to have to manually check each essay for plagiarism but now that it's automated I don't have to! I give it a 9/9 for quality tools and a 1/1 for aesthetic. Total 10/10!
-        </p>
-        <span>- Karen J. (High School Teacher)</span>
-        <p>
-          This was a very helpful tool when writing speeches for my Speech Writing Class. The sentiment and toxicity analysis helps me better gauge how my audience would feel during my presentation! Definitely recommend utilizing before a big presentation!
-        </p>
-        <span>- Smar T. (Harvard Student)</span>
-        <p>
-
-        </p>
-        <span>- Smar T. (Harvard Student)</span>
+        <div>
+          <p>
+            SpeechBuddy is wonderful! I used to have to manually check each essay for plagiarism but now that it's automated I don't have to! I give it a 9/9 for quality tools and a 1/1 for aesthetic. Total 10/10!
+          </p>
+          <span>- Karen J. (High School Teacher)</span>
+          <p>
+            This was a very helpful tool when writing speeches for my Speech Writing Class. The sentiment and toxicity analysis helps me better gauge how my audience would feel during my presentation! Definitely recommend utilizing before a big presentation!
+          </p>
+          <span>- Smar T. (Harvard Student)</span>
+          <p>
+            My dad like this alot.
+          </p>
+          <span>- Smar T. Jr. (Preschool Student)</span>
+        </div>
       </header>
 
     </div>
