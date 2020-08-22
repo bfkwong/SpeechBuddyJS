11493abec5412a4000ec3f0c2619d8cc7be43f69
Bryan Kwong <bfkwong@calpoly.edu>
2020-08-21 20:12:45 -0700

    fix: code smell

--- src/redux/reducer/textStoreReducer.js
+++ src/redux/reducer/textStoreReducer.js
@@ -2,6 +2,6 @@ const initialState = {
   text: []
 };
 
-export default function textStoreReducer(state = initialState, action) {
-  return state;
+export default function textStoreReducer(state, action) {
+  return state ? state : initialState;
 }
